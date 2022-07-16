0dc7fa5294bccd9a04ac21b159ec1e8cf891e167
Osezei <john.osezei@yahoo.com>
2022-07-16 19:14:57 +0100

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,25 +12,23 @@ export const Contact = () => {
           tact
         </h3>
       </div>
-      <section className="container text-center mt-48 animate__animated animate__fadeInUp container">
-        <div>
-          <h3 className="uppercase font-semibold text-xl mb-4">
-            Send me a message
-          </h3>
-          <a
-            className="font-bold text-4xl"
-            href="mailto:john.osezei@yahoo.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            john.osezei@yahoo.com
-          </a>
-          <h3 className="font-semibold mt-4 text-xl">
-            OR CONNECT WITH ME ON SOCIALS
-          </h3>
-          <div className="w-max mr-auto ml-auto text-2xl">
-            <Socials />
-          </div>
+      <section className="container text-center mt-48 animate__animated animate__fadeInUp">
+        <h3 className="uppercase text-center font-semibold text-lg mb-4">
+          Send me a message
+        </h3>
+        <a
+          className="container font-bold md:text-2xl lg:text-6xl"
+          href="mailto:john.osezei@yahoo.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          john.osezei@yahoo.com
+        </a>
+        <h3 className="font-semibold mt-4 text-lg text-center">
+          OR CONNECT WITH ME ON SOCIALS
+        </h3>
+        <div className="w-max mr-auto ml-auto text-2xl">
+          <Socials />
         </div>
       </section>
       <Navigation />
